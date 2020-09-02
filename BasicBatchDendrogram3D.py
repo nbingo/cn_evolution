f6324399fb358f5026adf5754f607d12b2519b5d
nbingo <5738735+nbingo@users.noreply.github.com>
2020-09-02 11:35:11 -0700

    Only reporting best BME tree.

--- BasicBatchDendrogram3D.py
+++ BasicBatchDendrogram3D.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(1, '/Users/noamringach/PycharmProjects/3dtrees')
+
 from agglomerate.batch_agglomerate_3d import BatchAgglomerate3D
 from BasicDendrogram3D import CTDataLoader
 from metrics.metric_utils import spearmanr_connectivity
@@ -5,7 +8,7 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    data = CTDataLoader(['mouse'], ['mouse'], orthologs=False)
+    data = CTDataLoader(['chicken'], orthologs=False)
     agglomerate = BatchAgglomerate3D(
         linkage_cell=['complete'],
         linkage_region=['homolog_avg'],
@@ -18,8 +21,13 @@ if __name__ == '__main__':
     agglomerate.agglomerate(data)
     best_agglomerators = agglomerate.get_best_agglomerators()
     pd.options.display.width = 0
-    best_agglomerators['BME'][1][2].view_tree3d()
-    print(best_agglomerators['BME'][1][0].region_dist_scale)
+    bme_as = best_agglomerators['BME'][1]
+    bme_as_scored = [bme_a.compute_tree_score('BME') for bme_a in bme_as]
+    best_bme_a = bme_as[np.argmin(bme_as_scored)]
+    print(best_bme_a.compute_tree_score('BME'))
+    print(best_bme_a.region_dist_scale)
+    print(best_bme_a.linkage_mat_readable)
+    best_bme_a.view_tree3d()
     # for metric in TREE_SCORE_OPTIONS:
     # for metric in ['BME']:
     #     print(f'Best {metric} score: {best_agglomerators[metric][0]}\n')
